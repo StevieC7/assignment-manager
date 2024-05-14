f173a2517f25456fd889c94c634abf9bdab6f40a
Steven Calhoun <stevenrcalhoun@outlook.com>
2024-05-14 17:37:37 -0500

    Add some inputs

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,41 @@
-import Image from "next/image";
-
+'use client';
+import { Button, TextField, Typography } from "@mui/material";
+import { useState } from "react";
 export default function Home() {
+    const [nurseList, setNurseList] = useState<string[]>([]);
+    const [nurseName, setNurseName] = useState<string>('');
     return (
         <main>
             <h1>Room Assigner</h1>
-            <input></input>
-        </main>
+            <TextField
+                placeholder="Nurse Name"
+                value={nurseName}
+                onChange={e => setNurseName(e.currentTarget.value)}
+            />
+            <Button
+                onClick={() => {
+                    setNurseList([...nurseList, nurseName])
+                    setNurseName('')
+                }}
+            >
+                Save
+            </Button>
+            {nurseList.map((nurse, id) => {
+                return (
+                    <>
+                        <Typography key={id}>
+                            {nurse}
+                        </Typography>
+                        <Button onClick={() => {
+                            const newList = [...nurseList];
+                            newList.splice(id, 1);
+                            setNurseList(newList);
+                        }}>
+                            Delete
+                        </Button>
+                    </>
+                )
+            })}
+        </main >
     );
 }
