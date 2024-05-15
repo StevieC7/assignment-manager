cc12d5aeb36050c0a793e314ce5e8b92e56775f1
Steven Calhoun <stevenrcalhoun@outlook.com>
2024-05-15 14:17:51 -0500

    Clean up

--- app/utils/algo.ts
+++ app/utils/algo.ts
@@ -16,8 +16,6 @@ export function roomMatcher(nurses: string[], rooms: Room[]) {
     let nursePointer = 0;
     let roomPointer = 0;
     while (nursePointer < nurses.length && roomPointer < rooms.length) {
-        // Edge cases:
-        // Nurse's only room is the last room
 
         // Nurse has no rooms, automatically they should be getting one
         if (nurseRooms.length === 0) {
@@ -29,27 +27,17 @@ export function roomMatcher(nurses: string[], rooms: Room[]) {
             }
             continue;
         }
+
         // Nurse can add more patients before going over average
         if (runningPatientCount + rooms[roomPointer].patientCount <= averageAssignments) {
-            // add the room to nurseRooms array
             nurseRooms.push(rooms[roomPointer]);
-            // add to the running patient count
             runningPatientCount += rooms[roomPointer].patientCount;
-            // advance the room pointer
             roomPointer++;
             continue;
         }
-        // nurse has no rooms and pointer is sitting on currently unused room
-        // use the room for this nurse
-        if (nurseRooms.length === 0 && roomPointer === rooms.length - 1) {
-            assignments.push({ nurse: nurses[nursePointer], rooms: [rooms[roomPointer]] })
-            roomPointer++;
-            continue;
-        }
-        // If we are here, this means we cannot add the current room to the assignment
-        // If there are no nurses left to consider, this would be an exception and we should store it and continue at the next room
+
+        // When we cannot add the current room to the assignment and there are no nurses left to consider, this would be an exception we should store, then continue at the next room
         if (nursePointer === nurses.length - 1 && runningPatientCount + rooms[roomPointer].patientCount > averageAssignments) {
-            console.log({ nurse: nurses[nursePointer], room: rooms[roomPointer], runningPatientCount })
             exceptions.push(rooms[roomPointer]);
             roomPointer++;
             if (roomPointer === rooms.length - 1) {
@@ -57,6 +45,7 @@ export function roomMatcher(nurses: string[], rooms: Room[]) {
             }
             continue;
         }
+
         assignments.push({ nurse: nurses[nursePointer], rooms: nurseRooms });
         runningPatientCount = 0;
         nursePointer++;
